73a5b54c3f1b80091bb6409fa2bc9e035b908700
Stan Chen <solve153@gmail.com>
2017-10-17 15:18:14 +0800

    Implement post author identification mechanism

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :content, presence: true
+  validates :content, :presence => true
   belongs_to :group
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
