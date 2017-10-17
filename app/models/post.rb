1311a160c148878aae651e38429b3fb411d4ff2b
Stan Chen <solve153@gmail.com>
2017-10-17 09:53:43 +0800

    Implement post author identification mechanism

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   validates :content, :presence => true
   belongs_to :group
+<<<<<<< cc2a665c416e6ffe59d7c3ef8589d26064847278
+=======
+  belongs_to :author, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(user)
+    user && user == author
+  end
+>>>>>>> Implement post author identification mechanism
 end
