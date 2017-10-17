cf11527a106c5043a064552824c0dcdcdb4d3aae
Stan Chen <solve153@gmail.com>
2017-10-17 15:07:45 +0800

    Add and set gem devise
    Add devise views
    
    Fix codereview issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017043740) do
-
-  create_table "group_users", force: :cascade do |t|
-    t.integer  "group_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20171016082910) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "user_id"
-    t.integer  "posts_count", default: 0
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -34,7 +25,6 @@ ActiveRecord::Schema.define(version: 20171017043740) do
     t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,7 +40,6 @@ ActiveRecord::Schema.define(version: 20171017043740) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
