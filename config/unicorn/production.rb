162ab84b0d8b7fc677622f7e6aa168cd1bdf37ea
Stan Chen <solve153@gmail.com>
2017-10-25 12:03:06 +0800

    modify unicorn setting to preload

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,3 +7,15 @@ pid "/home/apps/rails-101/shared/tmp/pids/unicorn.pid"
 
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
+
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiceRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
