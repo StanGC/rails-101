f4e92b8dc1e8aeaaa52161f6ac285c47333deb62
Stan Chen <solve153@gmail.com>
2017-10-25 10:45:51 +0800

    move unicorn.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :rbenv_ruby, "2.2.4"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
-set :branch, ENV['BRANCH'] if ENV['BRANCH']
+# set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,5 @@ server "54.238.141.40", user: "apps", roles: %w{web app}, my_property: :my_value
 role :app, %w{apps@54.238.141.40}
 role :web, %w{apps@54.238.141.40}
 role :db,  %w{apps@54.238.141.40}
+
+set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
